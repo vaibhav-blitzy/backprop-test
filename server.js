09e910a05a567cb8bddc61d60cfafe7e394e263c
Vaibhav Jaiswar <vaibhav@blitzy.com>
2025-04-18 16:36:10 +0530

    File: server.js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const http = require('http');
+const express = require('express');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const app = express();
+
+// Root endpoint handler - returns 'Hello, World!' for all HTTP methods
+app.all('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
 });
 
-server.listen(port, hostname, () => {
+// Evening endpoint handler - returns 'Good evening' for all HTTP methods
+app.all('/evening', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Good evening\n');
+});
+
+// Start the Express server
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
